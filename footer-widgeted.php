64d5dd8779ab6982f2fa2a21e607c31cdb28d7f6
James Byun <james@byuncreative.com>
2011-06-13 15:55:21 -0700

    initial commit with Twitter update

---
+++ footer-widgeted.php
@@ -0,0 +1,26 @@
+<div id="twitter_update_list"></div> 
+<div id="twitter_follow"><a href="http://twitter.com/dswordpress" target="_blank">follow me</a></div> 
+</div> 
+</div> 
+
+
+<div id="footer-widgets">
+
+	
+	<div id="Footer-1">
+		<?php if (!dynamic_sidebar('Footer #1')) : ?>
+			<?php endif; ?>
+	</div><!-- end -->
+	
+	<div id="Footer-2">
+		<?php if (!dynamic_sidebar('Footer #2')) : ?>
+		<?php endif; ?>
+	</div><!-- end -->
+	
+	<div id="Footer-3">
+		<?php if (!dynamic_sidebar('Footer #3')) : ?>
+		<?php endif; ?>
+	</div><!-- end -->
+
+
+</div>
